3ed7a1fe1cfc998052355817d54d5d803b03f1b0
Gilmar Pereira da Silva Júnior <juninhsilva@live.com>
2023-10-05 07:01:49 -0300

    comparação

---
+++ 03 - operadores/01 - matematicos.js
@@ -0,0 +1,14 @@
+let variavel;
+console.log(variavel);
+variavel = 6;
+console.log(variavel);
+variavel + 1;
+console.log(variavel);
+variavel--;
+console.log(variavel);
+variavel++;
+console.log(variavel);
+variavel*=2
+console.log(variavel);
+variavel/=3
+console.log(variavel);
---
+++ 03 - operadores/02 - comparacao.js
@@ -0,0 +1,8 @@
+let valor = 10;
+
+if(valor > 15) console.log("Valor é maior que 15")
+else console.log("Valor é menor que 15, valendo " + valor)
+
+let cpf = "111.222.333-44";
+if(cpf == "222.333.444-55") console.log("Os CPFs são iguais.")
+else console.log("Os CPFs não são iguais.")
