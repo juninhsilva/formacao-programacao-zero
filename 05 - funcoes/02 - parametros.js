69571cd7a3b7fd434ecfe93dfacc3a493f796cc1
Gilmar Pereira da Silva Júnior <juninhsilva@live.com>
2023-10-10 14:39:10 -0300

    função com parâmetros

--- 05 - funcoes/02 - parametros.js
+++ 05 - funcoes/02 - parametros.js
@@ -2,7 +2,7 @@ function torrar(tempo){
     console.log(`Torrando pão por ${tempo} minutos.`);
 }
 
-function torrar(tempo, tipo){
+function torrar(tempo=2.5, tipo="forma"){
     console.log(`Torrando pão ${tipo} por ${tempo} minutos.`);
 }
 
